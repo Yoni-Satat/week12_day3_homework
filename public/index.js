ae27975788e4aaa2fe15ca0b7b8187ad5aa1f9cc
Yoni Satat <yoni.satat@gmail.com>
2017-12-13 17:15:28 +0000

    created XML request and display list of beers with images

--- public/index.js
+++ public/index.js
@@ -1,5 +1,43 @@
 const app = function () {
+  const url = 'https://api.punkapi.com/v2/beers';
 
+  makeRequest(url, requestComplete);
+  // create makeRequest()
+  // create requestComplete aka the callback
+  // create a function to display html (beerInfo())
+
+
+}
+
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.send();
+  request.addEventListener('load', callback);
+}
+
+const requestComplete = function() {
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  beerInfo(beers);
+}
+
+const beerInfo = function(beers) {
+  const ul = document.querySelector('#beer-list');
+  beers.forEach(function(beer) {
+    const beerUl = document.createElement('ul');
+    const name = document.createElement('li');
+    const imgLi = document.createElement('li');
+    name.innerText = beer.name;
+    const image = document.createElement('img');
+    image.classList.add('beer-image');
+    image.src = beer.image_url;
+    imgLi.appendChild(image);
+    beerUl.appendChild(name);
+    beerUl.appendChild(imgLi);
+    ul.appendChild(beerUl);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', app);
